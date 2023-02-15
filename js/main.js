f34e34e072423d8260f15f55a66b26c9f5a1c4ba
Laura Izaguirre <laurar.izaguirre@gmail.com>
2023-02-15 01:52:00 -0600

    Create template

---
+++ js/main.js
@@ -0,0 +1,10 @@
+import '../assets/styles/style.scss';
+
+
+document.querySelector('#app').innerHTML = `
+  <div>
+  <h1>Este es el título de mi plantilla</h1>
+  <p>Esta es la descripción de mi plantilla</p>
+  </div>
+`;
+
